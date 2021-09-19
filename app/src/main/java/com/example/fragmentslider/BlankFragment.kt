e0473e322b935e3d6bf33fadf1f2c5aafb5c3cd4
Evgenii Chmutov <garret.evg@gmail.com>
2021-09-20 01:19:03 +0300

    finish

--- app/src/main/java/com/example/fragmentslider/BlankFragment.kt
+++ app/src/main/java/com/example/fragmentslider/BlankFragment.kt
@@ -1,12 +1,19 @@
 package com.example.fragmentslider
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.LinearLayout
+import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatTextView
+import androidx.fragment.app.Fragment
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+
 
 const val ARG_COUNTER = "counter"
 
@@ -24,14 +31,22 @@ class BlankFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         arguments?.takeIf { it.containsKey(ARG_COUNTER) }?.apply {
             val counterTextView: AppCompatTextView = view.findViewById(R.id.counter)
+            val inAppNotifyText: AppCompatTextView = view.findViewById(R.id.in_app_notify_text)
             counter = getInt(ARG_COUNTER)
             counterTextView.text = counter.toString()
+            inAppNotifyText.text = context?.resources?.getString(
+                R.string.notification_text,
+                counter
+            )
+
         }
         setClicks(view)
     }
     private fun setClicks(view: View){
         val plus = view.findViewById<FloatingActionButton>(R.id.fab_plus)
         val minus = view.findViewById<FloatingActionButton>(R.id.fab_minus)
+        val createNotify = view.findViewById<AppCompatButton>(R.id.create_notify)
+
         if (counter == 1)
             minus.visibility = View.GONE
         plus.setOnClickListener{
@@ -40,6 +55,32 @@ class BlankFragment : Fragment() {
         minus.setOnClickListener{
             (activity as MainActivity).deleteFragment(counter)
         }
+        createNotify.setOnClickListener{
+            (activity as MainActivity).createNotifications(counter)
+            showInAppNotify(view)
+
+        }
+    }
+
+    private fun showInAppNotify(view: View) {
+        val inAppNotify = view.findViewById<LinearLayout>(R.id.in_app_notify)
+        val animFadeIn: Animation = AnimationUtils.loadAnimation(
+            context,
+            R.anim.fade_in
+        )
+        val animFadeOut: Animation = AnimationUtils.loadAnimation(
+            context,
+            R.anim.fade_out
+        )
+        inAppNotify.startAnimation(animFadeIn)
+        inAppNotify.visibility = View.VISIBLE
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed(Runnable {
+            inAppNotify.startAnimation(animFadeOut)
+        }, 1000)
+        handler.postDelayed(Runnable {
+            inAppNotify.visibility = View.GONE
+        }, 1500)
     }
 
     companion object {
